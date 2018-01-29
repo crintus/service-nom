bd9209be9de5c3310775ed984146380f93ebda20
Johan <johandp92@gmail.com>
2018-01-29 17:29:41 +0200

    Updated config

--- config/plugins/secrets.py
+++ config/plugins/secrets.py
@@ -24,7 +24,7 @@ if not env_vars_loaded:
         pass
 
     os.environ["POSTGRES_HOST"] = 'localhost'
-    os.environ["POSTGRES_PORT"] = os.environ.get('LOCAL_POSTGRES_PORT', '5431')
+    os.environ["POSTGRES_PORT"] = os.environ.get('LOCAL_POSTGRES_PORT', '5432')
 
 # Add all project configurations that are stored in env variables:
 
