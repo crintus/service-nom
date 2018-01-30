429e23f7b74c86dacdd79f343e1d20a77c85f17e
Johan <johandp92@gmail.com>
2018-01-30 16:03:10 +0200

    Updated config

--- config/plugins/database.py
+++ config/plugins/database.py
@@ -2,18 +2,20 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
-        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
-        'PORT': os.environ.get('POSTGRES_PORT', 5432),
-        'OPTIONS': {
-            'connect_timeout': 25,
-        }
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:@postgres:5432/postgres',
+        conn_max_age=500
+    )
 }
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+#         'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+#         'PORT': os.environ.get('POSTGRES_PORT', 5432),
+#         'OPTIONS': {
+#             'connect_timeout': 25,
+#         }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
