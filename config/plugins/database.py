712ed3ae551f08be0656bce53424aef3d0a3c0a6
Johan <johandp92@gmail.com>
2018-01-30 16:22:54 +0200

    Updated config

--- config/plugins/database.py
+++ config/plugins/database.py
@@ -2,11 +2,16 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://postgres:@postgres:5432/postgres',
-        conn_max_age=500
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_PORT_5432_TCP_ADDR', 'postgres'),
+        'PORT': os.environ.get('POSTGRES_1_PORT_5432_TCP_PORT', 'vs')
+    }
 }
+
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'postgres',
 #         'USER': 'postgres',
@@ -17,11 +22,7 @@ DATABASES = {
 #             'connect_timeout': 25,
 #         }
 
-print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-print(dj_database_url.config(
-    default='postgres://postgres:@postgres:5432/postgres',
-    conn_max_age=500
-))
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+# {'NAME': 'postgres', 'USER': 'postgres', 'PASSWORD': '', 'HOST': 'postgres', 'PORT': 5432, 'CONN_MAX_AGE': 500, 'ENGINE': 'django.db.backends.postgresql_psycopg2'}
