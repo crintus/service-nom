2a60a7a9385dd3389a8b003b06ba4ac79fa1a0d2
Johan <johandp92@gmail.com>
2018-01-30 14:58:47 +0200

    Updated config

--- postgres_ready.sh
+++ postgres_ready.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# set -o errexit
-# set -o pipefail
+set -o errexit
+set -o pipefail
 
 
 # cmd="$@"
@@ -12,16 +12,13 @@ export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5431/p
 
 function postgres_ready(){
 python << END
-    import sys
-    import psycopg2
-    try:
-        conn = psycopg2.connect(
-            dbname="$POSTGRES_USER", user="$POSTGRES_USER",
-            password="$POSTGRES_PASSWORD", host="postgres"
-        )
-    except psycopg2.OperationalError:
-        sys.exit(-1)
-    sys.exit(0)
+import sys
+import psycopg2
+try:
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
 END
 }
 
