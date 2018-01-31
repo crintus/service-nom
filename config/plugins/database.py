1800ea6de10606c3561f607a8b7c33c6df73c00f
Johan <johandp92@gmail.com>
2018-01-31 10:02:01 +0200

    Updated config

--- config/plugins/database.py
+++ config/plugins/database.py
@@ -7,11 +7,13 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_PORT_5432_TCP_ADDR', 'postgres'),
-        'PORT': os.environ.get('POSTGRES_1_PORT_5432_TCP_PORT', '5432')
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'OPTIONS': {
+            'connect_timeout': 25,
+        }
     }
 }
 
-DATABASES['default'].update(
-    dj_database_url.config(conn_max_age=500)
-)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
