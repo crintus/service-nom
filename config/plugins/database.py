d7f8eda84fa69dc11098c011236cf112690644b4
Johan <johandp92@gmail.com>
2018-01-31 10:58:48 +0200

    Updated database to use dj when DATABASE_URL exists

--- config/plugins/database.py
+++ config/plugins/database.py
@@ -12,5 +12,5 @@ DATABASES = {
     }
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+if os.environ.get('DATABSE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=500)
